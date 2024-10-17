3c75f5a8e438a58e597390396d33f6e2b874502f
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-10-17 23:44:11 +0530

    Initial commit

---
+++ src/main/java/org/example/ticketingapp/TicketingAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.ticketingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/ticketingapp/TicketingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ticketingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
