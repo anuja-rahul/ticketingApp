f1233f894819dc5ace82aa504ac7bf7d9c0b0b52
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-10-17 23:45:17 +0530

    initial commit

--- src/main/java/org/example/ticketingapp/TicketingAppApplication.java
+++ src/main/java/org/example/ticketingapp/TicketingAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicketingAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TicketingAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingAppApplication.class, args);
+    }
 
 }
