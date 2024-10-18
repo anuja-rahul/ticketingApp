6db2537270032cbdaee16f558a33e4caa895cfc8
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-10-18 07:30:14 +0530

    changes

--- src/main/java/org/example/ticketingapp/test/TestEnv.java
+++ src/main/java/org/example/ticketingapp/test/TestEnv.java
@@ -1,9 +1,28 @@
 package org.example.ticketingapp.test;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
-public class TestEnv {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+@Component
+public class TestEnv implements CommandLineRunner {
 
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
+    @Value("${spring.datasource.username}")
+    private String datasourceUsername;
+
+    @Value("${spring.datasource.password}")
+    private String datasourcePassword;
+
+    @Override
+    public void run(String... args) throws Exception {
+//        System.out.println("Datasource URL: " + datasourceUrl);
+//        System.out.println("Datasource Username: " + datasourceUsername);
+//        System.out.println("Datasource Password: " + datasourcePassword);
+        System.out.println("Datasource URL: " + datasourceUrl);
+        System.out.println("Datasource Username: " + datasourceUsername);
+        System.out.println("Datasource Password: " + datasourcePassword.replaceAll(".", "*"));
     }
 }
