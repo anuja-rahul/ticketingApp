bc786fac5001bfb6497ee54b7df9cb61ec778aec
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-10-19 14:14:16 +0530

    changes

--- src/main/java/org/example/ticketingapp/test/TestEnv.java
+++ src/main/java/org/example/ticketingapp/test/TestEnv.java
@@ -1,12 +1,15 @@
 package org.example.ticketingapp.test;
 
 import org.example.ticketingapp.logger.MethodLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TestEnv implements CommandLineRunner {
+    Logger log = LoggerFactory.getLogger(TestEnv.class);
 
     @Value("${spring.datasource.url}")
     private String datasourceUrl;
@@ -20,8 +23,8 @@ public class TestEnv implements CommandLineRunner {
     @MethodLogger
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Datasource URL: " + datasourceUrl);
-        System.out.println("Datasource Username: " + datasourceUsername.replaceAll(".", "*"));
-        System.out.println("Datasource Password: " + datasourcePassword.replaceAll(".", "*"));
+        log.info("Datasource URL: {}", datasourceUrl);
+        log.info("Datasource Username: {}", datasourceUsername.replaceAll(".", "*"));
+        log.info("Datasource Password: {}", datasourcePassword.replaceAll(".", "*"));
     }
 }
