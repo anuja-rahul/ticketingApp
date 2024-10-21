c314ae0aef6d9be7f20e082db6172ca6b780554d
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-10-21 13:32:49 +0530

    ¯\_(ツ)_/¯

---
+++ src/main/java/org/example/ticketingapp/entity/Customer.java
@@ -0,0 +1,25 @@
+package org.example.ticketingapp.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+    @Column
+    private String email;
+    @Column(nullable = false, unique = true)
+    private String password;
+}
