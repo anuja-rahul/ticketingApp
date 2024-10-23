83f78eef33e3858758da5bbe157775e32987364c
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-10-23 23:48:56 +0530

    ¯\_(ツ)_/¯ implementing VendorEventConfig and associated CRUD operations

--- src/main/java/org/example/ticketingapp/controller/VendorEventConfigController.java
+++ src/main/java/org/example/ticketingapp/controller/VendorEventConfigController.java
@@ -1,13 +1,12 @@
 package org.example.ticketingapp.controller;
 
 
-import org.example.ticketingapp.dto.VendorEventConfigDTO;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/config")
-public class VendorConfigController {
+public class VendorEventConfigController {
 
 //    private VendorEventConfigDTO vendorConfigDTO = new VendorEventConfigDTO();
 
