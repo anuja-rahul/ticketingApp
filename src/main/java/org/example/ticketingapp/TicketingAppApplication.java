899d1745ca6a0863baddcf14f950a9a14ac0696c
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-10-26 20:07:42 +0530

    ¯\_(ツ)_/¯ Testing some stuff

--- src/main/java/org/example/ticketingapp/TicketingAppApplication.java
+++ src/main/java/org/example/ticketingapp/TicketingAppApplication.java
@@ -6,7 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicketingAppApplication {
     public static void main(String[] args) {
+
+
+
         SpringApplication.run(TicketingAppApplication.class, args);
+        System.out.println("\nThis is before launching my spring application\n");
+        System.out.println("\nHello\n");
     }
 
 }
