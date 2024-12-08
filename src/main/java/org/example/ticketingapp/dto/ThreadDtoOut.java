ddd87df0cd342c6383c02f880bec6bfd18074406
Anuja Gunasinghe <a.r.gunasinghe@gmail.com>
2024-12-08 12:10:25 +0530

    finished threadPoolStat endpoint and service

--- src/main/java/org/example/ticketingapp/dto/ThreadDtoOut.java
+++ src/main/java/org/example/ticketingapp/dto/ThreadDtoOut.java
@@ -1,4 +1,16 @@
 package org.example.ticketingapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ThreadDtoOut {
+    private String threadName;
+    private int activeThreads;
+    private int idleThreads;
+    private int totalThreads;
+
 }
--- src/main/java/org/example/ticketingapp/service/impl/ThreadPoolServiceImpl.java
+++ src/main/java/org/example/ticketingapp/service/impl/ThreadPoolServiceImpl.java
@@ -7,6 +7,7 @@ import org.example.ticketingapp.dto.ThreadDtoOut;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,6 +21,54 @@ public class ThreadPoolServiceImpl {
 
 
     public List<ThreadDtoOut> getThreadStatus() {
-        return null;
+        ArrayList<Integer> taskList = getStatus(taskExecutor);
+        ArrayList<Integer> ticketList = getStatus(ticketExecutor);
+        ArrayList<Integer> vendorList = getStatus(vendorExecutor);
+        ArrayList<Integer> customerList = getStatus(customerExecutor);
+
+        List<ThreadDtoOut> threadDtoOutList = new ArrayList<>();
+
+        ThreadDtoOut task = new ThreadDtoOut(
+                "taskExecutor",
+                taskList.get(0),
+                taskList.get(1),
+                taskList.get(2)
+                );
+        ThreadDtoOut ticket = new ThreadDtoOut(
+                "ticketExecutor",
+                ticketList.get(0),
+                ticketList.get(1),
+                ticketList.get(2)
+        );
+        ThreadDtoOut vendor = new ThreadDtoOut(
+                "vendorExecutor",
+                vendorList.get(0),
+                vendorList.get(1),
+                vendorList.get(2)
+        );
+        ThreadDtoOut customer = new ThreadDtoOut(
+                "customerExecutor",
+                customerList.get(0),
+                customerList.get(1),
+                customerList.get(2)
+        );
+
+        threadDtoOutList.add(task);
+        threadDtoOutList.add(ticket);
+        threadDtoOutList.add(vendor);
+        threadDtoOutList.add(customer);
+
+
+        return threadDtoOutList;
+    }
+
+    private ArrayList<Integer> getStatus(ThreadPoolTaskExecutor executor) {
+        ArrayList<Integer> threadList = new ArrayList<>();
+
+        threadList.add(threadPoolConfig.getActiveThreads(executor));
+        threadList.add(threadPoolConfig.getIdleThreads(executor));
+        threadList.add(threadPoolConfig.getTotalThreads(executor));
+
+        return threadList;
     }
 }
